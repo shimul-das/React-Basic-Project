ab77019cb03e8f3202262aad4a6266f790028fcb
Shimul Das <shimul.191002110@gmail.com>
2023-03-29 15:15:35 +0600

    json data in file

--- react-storage-and-handaler/src/components/Cosmatics/cosmatics.js
+++ react-storage-and-handaler/src/components/Cosmatics/cosmatics.js
@@ -4,14 +4,39 @@ import add, { multiply } from './utilities/calculate';
 //import add from './utilities/calculate';
 
 const Cosmatics = () => {
+    //generate json data 
 const cosmatics=[
-    {id: 1, name: "alta", price: 110},
-    {id: 2, name: "Sunu", price: 200},
-    {id: 3, name: "Churi", price: 250},
-    {id: 4, name: "Ciruni", price: 450},
-    {id: 5, name: "Nailpoplish", price: 450},
-    {id: 6, name: "Kajol", price: 550},
-]
+    {
+      "id": "6423fc88778ef0c9d649e365",
+      "price": 159,
+      "name": "Newton Hodges"
+    },
+    {
+      "id": "6423fc88ff3ac5747d3bf86d",
+      "price": 261,
+      "name": "Frances Chapman"
+    },
+    {
+      "id": "6423fc88b92236bd15bc21c1",
+      "price": 377,
+      "name": "Ginger Key"
+    },
+    {
+      "id": "6423fc8889daf190420e8300",
+      "price": 370,
+      "name": "Merle Donovan"
+    },
+    {
+      "id": "6423fc88a00616d418d64659",
+      "price": 158,
+      "name": "Audra Garrison"
+    },
+    {
+      "id": "6423fc88389283b3609bee53",
+      "price": 93,
+      "name": "Sharron Mcpherson"
+    }
+  ]
   return (
     <div>
         <h1>This is cosmatics component</h1>
