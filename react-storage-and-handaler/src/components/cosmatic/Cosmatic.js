d72f286941331262affa2db06960fd8cb835a450
Shimul Das <shimul.191002110@gmail.com>
2023-03-29 16:31:41 +0600

    event handler to add product in cart

--- react-storage-and-handaler/src/components/cosmatic/Cosmatic.js
+++ react-storage-and-handaler/src/components/cosmatic/Cosmatic.js
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import './cosmatic.css' 
 const Cosmatic = (props) => {
     // Here need Destucturing to remove long variable pass in element
     const {name,price,id}=props.cosmatic
+    const addtoProduct=(id)=>{
+        console.log('Product Added',id)
+    }
+    const addToCartWithParameter=()=>addtoProduct(id);
   return (
-    <div>
+    <div className='product'>
         <h2>Buy This:{name}</h2>
         <p>Only For:${price}</p>
         <p><small>It has a Id:{id}</small></p>
+        <button onClick={addToCartWithParameter}>Add To Cart</button>
+        <button onClick={()=>addtoProduct(id)}>Add To Cart: Short Cut Function</button>
     </div>
   )
 }
