7fa7d9c9fc97c3b88511f94b7ae7b3ccc508fcce
Shimul Das <shimul.191002110@gmail.com>
2023-03-29 19:49:10 +0600

    updated shopping cart by using local storage

--- react-storage-and-handaler/src/components/Cosmatics/utilities/fakedb.js
+++ react-storage-and-handaler/src/components/Cosmatics/utilities/fakedb.js
@@ -1,15 +1,38 @@
 //use local storage manage card data.
 const cartToDb=(id)=>{
-    const quantity=localStorage.getItem(id);
-    if(quantity){
-        console.log('Item already exit');
-        const newquanty=parseInt(quantity)+1;
-        localStorage.setItem(id,newquanty);
+    let shoppingCart;
+    localStorage.setItem(id,JSON.stringify(shoppingCart))
+    //get the shopping cart
+    const storedCart=localStorage.getItem('shopping-cart');
+    if(storedCart){
+       shoppingCart=JSON.parse(storedCart);
     }
     else{
-        console.log('new item added')
-        localStorage.setItem(id,1)
+        shoppingCart={};
     }
+    //add quantity
+    const quantity=shoppingCart[id]    
+    if(quantity){
+        console.log('Item already exit');
+        const newquanty=quantity+1;
+        shoppingCart[id]=newquanty
+        // localStorage.setItem(id,newquanty);
+    }
+    else{
+        shoppingCart[id]=1;
+    }
+
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+
+    // if(quantity){
+    //     console.log('Item already exit');
+    //     const newquanty=parseInt(quantity)+1;
+    //     localStorage.setItem(id,newquanty);
+    // }
+    // else{
+    //     console.log('new item added')
+    //     localStorage.setItem(id,1)
+    // }
     
 }
 
