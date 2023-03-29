bb65226f967b50ea0d08f01e916163cc2bf84b45
Shimul Das <shimul.191002110@gmail.com>
2023-03-29 15:45:59 +0600

    dataset from online or json format file

--- react-storage-and-handaler/src/components/Cosmatics/cosmatics.js
+++ react-storage-and-handaler/src/components/Cosmatics/cosmatics.js
@@ -1,42 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Cosmatic from '../cosmatic/Cosmatic';
 import add, { multiply } from './utilities/calculate';
 //import add from './utilities/calculate';
 
 const Cosmatics = () => {
-    //generate json data 
-const cosmatics=[
-    {
-      "id": "6423fc88778ef0c9d649e365",
-      "price": 159,
-      "name": "Newton Hodges"
-    },
-    {
-      "id": "6423fc88ff3ac5747d3bf86d",
-      "price": 261,
-      "name": "Frances Chapman"
-    },
-    {
-      "id": "6423fc88b92236bd15bc21c1",
-      "price": 377,
-      "name": "Ginger Key"
-    },
-    {
-      "id": "6423fc8889daf190420e8300",
-      "price": 370,
-      "name": "Merle Donovan"
-    },
-    {
-      "id": "6423fc88a00616d418d64659",
-      "price": 158,
-      "name": "Audra Garrison"
-    },
-    {
-      "id": "6423fc88389283b3609bee53",
-      "price": 93,
-      "name": "Sharron Mcpherson"
-    }
-  ]
+    //generate json data from json-generte.com
+    // now we will load data using fetch
+    //To fetch data we have to load data for this reason we have to declare state.
+    //as we will load data from array thats why use state will be empy array.
+const [cosmatics,setcosmatics]=useState([])
+//for load we have to use useEffect
+//make free mock api using online tool
+useEffect(()=>{
+    fetch('dataset.json')
+    .then(res=>res.json())
+    .then(data=>setcosmatics(data))
+},[])
   return (
     <div>
         <h1>This is cosmatics component</h1>
