028b8e2d2016164a8d74bea68eda8ce71f746281
Shimul Das <shimul.191002110@gmail.com>
2023-03-30 18:35:02 +0600

    data retrived from local storage

--- ema-jhon-simple/src/Components/Cart/Cart.jsx
+++ ema-jhon-simple/src/Components/Cart/Cart.jsx
@@ -2,19 +2,23 @@ import React from 'react'
 import './cart.css'
 
 const Cart = ({cart}) => {
-    console.log(cart);
+    //console.log(cart);
     let total=0;
+    let totalShipping=0;
     for(let product of cart){
       total= total+product.price
+      totalShipping=totalShipping+product.shipping;
     }
+    const tax=(total*5)/100;
+    const grand_total=total+tax
   return (
     <div className='cart'>
         <h4>Order Summary</h4>
         <p className='cart-item'>Added Product:{cart.length}</p>
-        <p className='cart-item'>Total Price:{total}</p>
-        <p className='cart-item'>Total Shipping Charge:</p>
-        <p className='cart-item'>Tax: </p>
-        <p className='grand-total'>Grand Total: </p>
+        <p className='cart-item'>Total Price:${total}</p>
+        <p className='cart-item'>Total Shipping Charge:${totalShipping}</p>
+        <p className='cart-item'>Tax: ${tax}</p>
+        <p className='grand-total'>Grand Total:${grand_total} </p>
 
     </div>
   )
